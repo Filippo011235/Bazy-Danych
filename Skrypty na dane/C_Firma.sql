fb68b07ca6372a1ea2efa15d966d491657e17690
Filippo011235 <fadamcewicz1@gmail.com>
2020-03-31 15:27:58 +0200

    Powinno działać

---
+++ Skrypty na dane/C_Firma.sql
@@ -0,0 +1,7 @@
+INSERT INTO mydb.firma 
+VALUES 	("Kokos",6.34,"Aluminium"),
+		("Recykl",1.23,"Karton"),
+		("PolRec",0.67,"PET"),
+		("Bakar",9.99,"Metale");
+
+SELECT * FROM mydb.firma;
---
+++ Skrypty na dane/D_Kontener.sql
@@ -0,0 +1,7 @@
+INSERT INTO mydb.kontener (Waga,Material_Nazwa)
+VALUES 	(5141,"Aluminium"),
+		(1235,"Karton"),
+        (4325,"PET"),
+        (2222,"Metale");
+        
+SELECT * FROM mydb.kontener;
