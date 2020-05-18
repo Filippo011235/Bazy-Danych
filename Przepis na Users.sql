3e0b727df0167649fb61b4ce8d067961ac18c69c
Filippo011235 <37882752+Filippo011235@users.noreply.github.com>
2020-05-18 18:18:04 +0200

    Finaly

---
+++ Przepis na Users.sql
@@ -0,0 +1,59 @@
+CREATE USER IF NOT EXISTS inz@localhost IDENTIFIED BY 'inz';
+
+GRANT SELECT, UPDATE, INSERT
+ON mydb.Material
+TO inz@localhost;
+
+GRANT SELECT, UPDATE, INSERT, DELETE
+ON mydb.Odpad
+TO inz@localhost;
+
+-- GRANT 
+--    SELECT (Nr_rejestracyjny)
+-- ON mydb.Smieciarka 
+-- TO inz@localhost;
+
+-- 
+GRANT SELECT
+ON mydb.Kontener
+TO inz@localhost;
+
+GRANT SELECT
+ON mydb.Smieciarka
+TO inz@localhost;
+
+
+-- ------------------------------------------
+CREATE USER IF NOT EXISTS handel@localhost IDENTIFIED BY 'handel';
+
+GRANT SELECT, UPDATE
+ON mydb.Kontener
+TO handel@localhost;
+
+GRANT SELECT, INSERT
+ON mydb.Sprzedaze
+TO handel@localhost;
+
+GRANT SELECT, UPDATE, INSERT, DELETE
+ON mydb.Firma
+TO handel@localhost;
+
+-- ------------------------------------------
+CREATE USER IF NOT EXISTS oper@localhost IDENTIFIED BY 'oper';
+
+GRANT SELECT, UPDATE, INSERT
+ON mydb.Smieciarka
+TO oper@localhost;
+
+GRANT SELECT, UPDATE, INSERT, DELETE
+ON mydb.Odpad
+TO oper@localhost;
+
+GRANT SELECT, UPDATE, INSERT
+ON mydb.Kontener
+TO oper@localhost;
+
+GRANT SELECT
+ON mydb.Material
+TO oper@localhost;
+
